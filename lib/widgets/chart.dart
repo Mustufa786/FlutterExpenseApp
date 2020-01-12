989af2430c937f7cbc20a86374b5b0500c90bd5d
Mustufa786 <mustufa_ayub@hotmail.com>
2020-01-12 23:35:15 +0500

    some changes

---
+++ lib/widgets/chart.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Chart extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      margin: EdgeInsets.all(10),
+      child: Row(
+        children: <Widget>[
+        ],
+      ),
+    );
+  }
+}
